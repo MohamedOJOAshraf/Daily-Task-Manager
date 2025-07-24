5fa073049376c7587719408ca0672aba1c3eb4b1
unknown <mohamed20252018ii@gmail.com>
2025-07-23 23:52:51 -0700

    post task

--- front-end/src/components/AddTask.js
+++ front-end/src/components/AddTask.js
@@ -1,13 +1,28 @@
+import { useState } from 'react'
 import './mainPage.css'
+import axios from 'axios'
 
 function AddTask() {
+    const api_url = 'http://localhost:4000'
+
+    const [taskName, setTaskName] = useState('')
+
+    const eventSubmit = (e) => {
+        e.preventDefault()
+
+        axios.post(`${api_url}/createTask`, {
+            name: taskName
+        }).then(res => console.log(res))
+    }
     return (
-        <div className='addTask'>
+        <div className='addTask' >
 
             <div className='input-box'>
-                <input type='text' placeholder='Task Name' />
-                <div></div>
-                <button><i class="fa-solid fa-square-caret-right"></i></button>
+                <form onSubmit={eventSubmit}>
+                    <input type='text' placeholder='Task Name' onChange={(e) => setTaskName(e.target.value)} />
+                    <div></div>
+                    <button type='submit'>submit</button>
+                </form>
             </div>
         </div>
     )
